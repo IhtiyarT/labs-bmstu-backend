07f6882c2fbe6847793bc952d0b8a8bf73c9d68a
IhtiyarT <turdievihtiyar@gmail.com>
2025-09-17 15:21:43 +0300

    tables changed

--- internal/app/ds/Users.go
+++ internal/app/ds/Users.go
@@ -1,7 +1,7 @@
 package ds
 
 type Users struct {
-	UserID uint `json:"id" gorm:"primaryKey"`
+	UserID uint `json:"user_id" gorm:"primaryKey"`
 	Login string `json:"login" gorm:"type:varchar(100);unique"`
 	Password string `json:"-" gorm:"type:varchar(100)"`
 	IsAdmin bool `json:"is_admin"`
